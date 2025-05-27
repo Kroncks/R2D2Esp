f93506588cf024baedbbeda6be6024b1acc9555f
Kroncks <161207379+Kroncks@users.noreply.github.com>
2025-05-28 01:03:20 +0200

    ajout du readme

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,47 @@ const int puissance_tourne_recul_court = 20;
 
 WebServer server(80);
 
+// README 
+
+void handleReadme() {
+  server.send(200, "text/html", R"rawliteral(
+    <!DOCTYPE html><html lang="fr"><head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>R2D2</title>
+    </head><body>
+    <!-- README CONVERTI AVEC https://dillinger.io/ -->
+
+    <h1 class="code-line" data-line-start=0 data-line-end=1 ><a id="Controle_du_R2D2_par_ESP32_0"></a>Controle du R2D2 par ESP32</h1>
+    <h2 class="code-line" data-line-start=2 data-line-end=3 ><a id="Acceder_a_linterface_de_commande_2"></a>Acceder a l’interface de commande</h2>
+    <ul>
+    <li class="has-line-data" data-line-start="3" data-line-end="4">Allumer l’ESP32</li>
+    <li class="has-line-data" data-line-start="4" data-line-end="5">Se connecter au reseau R2D2 avec le code : <code>BONSOIRS</code></li>
+    <li class="has-line-data" data-line-start="5" data-line-end="6">Ouvrir <a href="http://192.168.4.1/">http://192.168.4.1/</a> sur un navigateur</li>
+    <li class="has-line-data" data-line-start="6" data-line-end="8">Cliquer sur le bouton <code>Controles</code></li>
+    </ul>
+    <h2 class="code-line" data-line-start=8 data-line-end=9 ><a id="Verifier_que_la_connexion_fonctionne_8"></a>Verifier que la connexion fonctionne</h2>
+    <ul>
+    <li class="has-line-data" data-line-start="9" data-line-end="10">Appuyer sur le bouton au centre de la croix directionelle</li>
+    <li class="has-line-data" data-line-start="10" data-line-end="12">Verifier qu’une LED bleu s’allume bien sur l’ESP32</li>
+    </ul>
+    <h2 class="code-line" data-line-start=12 data-line-end=13 ><a id="Connexion_de_lESP32_au_driver_moteur_Sabertooth_12"></a>Connexion de l’ESP32 au driver moteur Sabertooth</h2>
+    <ul>
+    <li class="has-line-data" data-line-start="13" data-line-end="14">Relier la pin TX2 de l’ESP32 a l’entree S1 du Sabertooth</li>
+    <li class="has-line-data" data-line-start="14" data-line-end="16">Ne pas modifier les reglages du Sabertooth ( 010100 -&gt; Serial 9600 )</li>
+    </ul>
+    <h2 class="code-line" data-line-start=16 data-line-end=17 ><a id="Connexion_des_moteurs_au_Sabertooth_16"></a>Connexion des moteurs au Sabertooth</h2>
+    <ul>
+    <li class="has-line-data" data-line-start="17" data-line-end="18">Connecter le moteur droit sur M1</li>
+    <li class="has-line-data" data-line-start="18" data-line-end="19">Connecter le moteur gauche sur M2</li>
+    </ul>
+
+    <!-- FIN README -->
+    </body></html>
+  )rawliteral");
+}
+
+
 //Pages 
 
 void handleRoot() {
@@ -401,6 +442,7 @@ void setup() {
     
     server.on("/", HTTP_GET, handleRoot);
     server.on("/bonsoir", HTTP_GET, handleBonsoir);
+    server.on("/readme", HTTP_GET, handleReadme);
 
     server.on("/controls", HTTP_GET, handleControls); // affichage 
     server.on("/comand", HTTP_POST, handleComand); // envoi des comandes 
